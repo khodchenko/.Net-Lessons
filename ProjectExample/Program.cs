4973812a6050412b96b17a51ae4f7e4ce2cfd3d7
BORIS PIDRIS <khodch3nk0@gmail.com>
2022-07-04 17:46:22 +0200

    Added

---
+++ ProjectExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.Write("Enter kilometer value:");
+            int kilometers = Convert.ToInt32(Console.ReadLine());
+
+            int meters = kilometers * 1000;
+
+            Console.Write("Meters amount:"+ meters);
+                  
+                   
+                       
+        }
+    }
+}
